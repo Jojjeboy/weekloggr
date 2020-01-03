346a7d9522667c4d83d46674ed8e71af2d4c7a05
Johannes Kärnstam <johannes.karnstam@havochvatten.se>
2020-01-03 10:42:32 +0100

    weeknr

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 require 'flight/Flight.php';
 
 Flight::route('/', function(){
-    //echo 'hello world!';
+    
+
     Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=weekloggr','root','mysql'));
     $db = Flight::db();
     $x = $db->query("SELECT * FROM `weekloggr`")->fetchAll(PDO::FETCH_ASSOC);
     Flight::render('start.php', array('weeklogs' => $x));
-
-
 });
+
+Flight::route('/addlog', function(){
+    $wl = new Weekloggr();
+    $text = Flight::request()->query['text'];
+    $weeknr = $wl->getWeekNr();
+});
+
+class Weekloggr{
+    public static function getWeekNr(){
+        $dt = new DateTime();
+        $weeknr = $dt->format("W");
+        if($weeknr[0] == "0"){
+            $weeknr = $weeknr[1];
+        }
+        
+        return $weeknr;
+    }
+}
 
 Flight::start();
