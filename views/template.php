ff335fb2a4f0e150bcd2bbd472063f7890fa4494
Johannes Kärnstam <johannes.karnstam@havochvatten.se>
2020-01-08 14:52:13 +0100

    Copy text function

--- views/template.php
+++ views/template.php
@@ -9,8 +9,8 @@
 
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="<?php echo $base_url;?>/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo $base_url;?>/style.css">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/style.css">
 
     <meta name="theme-color" content="#563d7c">
 
@@ -21,41 +21,40 @@
         <div class="mt-1">
             <h1>Weekloggr</h1>
             <?php
-        $weeknr = 0;
-        $first = true;
-        foreach ($weeklogs as $row) {
-            if ($weeknr != $row['weeknr']) {
-                if ($first == false) {
-                    echo '</div></ul>';
-                }
-                echo '<div class="card m-3">';
-                echo '<ul class="mb-3 list-group list-group-flush">';
-                $currWeekClass = $currentWeekNr == $row['weeknr'] ? ' sameAsCurrent' : '';
-                echo '<h6 class="m-2 ml-3'. $currWeekClass .'">Vecka ' . $row['weeknr'];
-                echo ' <button type="button" class="btn btn-link d-none showHidden" onclick="toggleArchived(this, '. $row['is_visible'] .')">Visa arkiverade</button></h6>';
-                $weeknr = $row['weeknr'];
-                $first = false;
-            }
-            $showArchived = $row['is_visible'];
-            $newStatus = $row['is_visible'] == 1 ? 0 : 1;
-            echo '<li class="list-group-item show-'. $showArchived .'">';
-            echo $row['text'];
-            ?>
-            <span class="dropup">
-                <a class="nav-link navbar-brand active dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
+            $weeknr = 0;
+            $first = true;
+            foreach ($weeklogs as $row) {
+                if ($weeknr != $row['weeknr']) {
+                    if ($first == false) {
+                        echo '</div></ul>';
+                    }
+                    echo '<div class="card m-3">';
+                    echo '<ul class="mb-3 list-group list-group-flush">';
+                    $currWeekClass = $currentWeekNr == $row['weeknr'] ? ' sameAsCurrent' : '';
+                    echo '<h6 class="m-2 ml-3' . $currWeekClass . '">Vecka ' . $row['weeknr'] . '</h6>';
+                    // echo ' <button type="button" class="btn btn-link d-none showHidden" onclick="toggleArchived(this, '. $row['is_visible'] .')">Visa arkiverade</button>';
+                    $weeknr = $row['weeknr'];
+                    $first = false;
+                } 
+                ?>
+                <li class="list-group-item">
+                <span class="dropup">
+                    <a class="navbar-brand active dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
                     <div class="dropdown-menu" aria-labelledby="dropdown10">
-                        <a class="dropdown-item small" href="<?php echo $base_url .'/delete/'. $row['id']; ?>">Ta bort</a>
-                        <a class="dropdown-item small" href="<?php echo $base_url .'/setstatus/'. $row['id'] . '/' . $newStatus; ?>"><?php echo $newStatus == 0 ? 'Arkivera' : 'Avarkivera';?></a>
+                        <a class="dropdown-item small warning" href="<?php echo $base_url . '/delete/' . $row['id']; ?>">Ta bort</a>
+                        <button type="button" class="btn small btn-link copybtn" onclick="copyToInput(this, <?php echo $row['id']; ?>)">Kopiera till inmatningsfält</button>
                     </div>
                 </span>
-            <?php
-            echo '</li>';
-        }
-        ?>
+                <span class="text-<?php echo $row['id']; ?>"> 
+                <?php echo $row['text']; ?></span>
+                </li>
+                <?php 
+            }
+            ?>
         </div>
     </div>
     <nav class="navbar fixed-bottom navbar-expand navbar-dark bg-dark">
-        
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -69,20 +68,20 @@
                     </div>
                 </li>
             </ul>
-            <form method="POST" name="addweeklog" action="<?php echo $base_url;?>/addlog">
+            <form method="POST" name="addweeklog" action="<?php echo $base_url; ?>/addlog">
                 <input class="form-control" name="weeklog" type="text" placeholder="Log work done">
                 <button type="submit" class="d-none">Skicka</button>
             </form>
-        </form>
+            </form>
         </div>
     </nav>
 
 
-    <script src="<?php echo $base_url;?>/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="<?php echo $base_url;?>/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="<?php echo $base_url;?>/bootstrap.min.js"></script>
+    <script src="<?php echo $base_url; ?>/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="<?php echo $base_url; ?>/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="<?php echo $base_url; ?>/bootstrap.min.js"></script>
 
-    <script src="<?php echo $base_url;?>/weeklog.js"></script>
+    <script src="<?php echo $base_url; ?>/weeklog.js"></script>
 </body>
 
 </html>
--- weeklog.js
+++ weeklog.js
@@ -1,37 +1,11 @@
 window.scrollTo(0, document.body.scrollHeight);
 
-
-let allHiddenLi = document.querySelectorAll("li.show-0");
-for (var i = 0; i < allHiddenLi.length; ++i) {
-    allHiddenLi[i].parentNode.firstElementChild.firstElementChild.classList.remove('d-none');
+function copyToInput(btn, id){
+    console.log(btn);
+    const txt = document.querySelector('.text-' + id).textContent.trim();
+    document.querySelector('form input').setAttribute('value', txt);
 }
 
-function toggleArchived(btn, status) {
-    let allHiddenLi = btn.parentNode.parentNode.querySelectorAll('li.show-0');
-    if(status === 1){
-        for (var i = 0; i < allHiddenLi.length; ++i) {
-            addClass(allHiddenLi[i], 'showAnyway');
-        }
-        btn.setAttribute('onclick', "toggleArchived(this, 0)");
-        btn.innerText = 'Göm arkiverade';
-    }
-    else {
-        for (var i = 0; i < allHiddenLi.length; ++i) {
-            removeClass(allHiddenLi[i], 'showAnyway');
-        }
-        btn.setAttribute('onclick', "toggleArchived(this, 1)");
-        btn.innerText = 'Visa arkiverade';
-    }
-}
-
-function hideArchived(btn) {
-    let allHiddenLi = btn.parentNode.parentNode.querySelectorAll('li.show-0');
-    for (var i = 0; i < allHiddenLi.length; ++i) {
-        removeClass(allHiddenLi[i], 'showAnyway');
-    }
-    document.querySelector('button.showHidden').innerText = 'Visa arkiverade';
-    
-}
 
 function addClass(ele, cls) {
     if (!hasClass(ele, cls)) ele.className += " " + cls;
@@ -46,18 +20,3 @@ function removeClass(ele, cls) {
 function hasClass(ele, cls) {
     return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
 }
-/*
-u('form').handle('submit', async e => {
-    log('Sending... ');
-    const form = u(e.target);
-    try {
-        await fetch(form.attr('action'), {
-            method: form.attr('method'),
-            body: form.serialize()
-        });
-        log("Awesome! (:");
-    } catch (error) {
-        log("Error: " + error);
-    }
-});
-*/
