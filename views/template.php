c03c37421e81857509551d6e89a270db372765f9
Johannes Kärnstam <snack@johanneskarnstam.se>
2020-01-28 02:40:11 +0100

    html validation

--- views/template.php
+++ views/template.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" class="h-100">
+<html lang="sv" class="h-100">
 
 <head>
     <meta charset="utf-8">
@@ -34,7 +34,7 @@
 </head>
 
 <body>
-    
+
     <div class="container-fluid">
         <div class="mt-1">
             <h1><a href="/">Weekloggr</a></h1>
@@ -45,11 +45,11 @@
                 if ($weeknr != $row['weeknr']) {
                     $weeknr = $row['weeknr'];
                     if ($first == false) {
-                        echo '</div></ul>';
+                        echo '</ul></div>';
                     }
                     echo '<div class="card m-3">';
                     $currWeekClass = $currentWeekNr == $weeknr ? ' sameAsCurrent' : ''; ?>
-                    <h6 class="m-2 ml-3 <?php echo $currWeekClass;?> ">
+                    <h6 class="m-2 ml-3 <?php echo $currWeekClass; ?> ">
                         <a data-toggle="collapse" href="#week<?php echo $row['weeknr'] ?>" role="button" aria-expanded="false" aria-controls="week<?php echo $weeknr ?>">Vecka <?php echo $row['weeknr']; ?></a>
                     </h6>
                 <?php
@@ -59,25 +59,25 @@
                 }
                 ?>
                 <li class="list-group-item">
-                    <span class="dropup">
-                        <a class="navbar-brand active" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <div class="dropup">
+                        <a class="navbar-brand active" href="#" id="dropdown-<?php echo $row['id']; ?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <svg class="bi bi-box-arrow-up-right" width="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M3.5 15A1.5 1.5 0 005 16.5h8a1.5 1.5 0 001.5-1.5v-4a.5.5 0 00-1 0v4a.5.5 0 01-.5.5H5a.5.5 0 01-.5-.5V7a.5.5 0 01.5-.5h4a.5.5 0 000-1H5A1.5 1.5 0 003.5 7v8zm7-11a.5.5 0 01.5-.5h5a.5.5 0 01.5.5v5a.5.5 0 01-1 0V4.5H11a.5.5 0 01-.5-.5z" clip-rule="evenodd"></path>
                                 <path fill-rule="evenodd" d="M16.354 3.646a.5.5 0 010 .708l-8 8a.5.5 0 01-.708-.708l8-8a.5.5 0 01.708 0z" clip-rule="evenodd"></path>
                             </svg>
 
                         </a>
-                        <div class="dropdown-menu nisse" aria-labelledby="dropdown10">
+                        <div class="dropdown-menu" aria-labelledby="dropdown-<?php echo $row['id']; ?>">
                             <a class="dropdown-item small warning" href="<?php echo $base_url . '/delete/' . $row['id']; ?>">Ta bort</a>
                             <button type="button" class="btn small btn-link copybtn" onclick="copyToInput(this, <?php echo $row['id']; ?>, true)">Redigera</button>
                             <button type="button" class="btn small btn-link copybtn" onclick="copyToInput(this, <?php echo $row['id']; ?>)">Kopiera till inmatningsfält</button>
                         </div>
-                    </span>
+                    </div>
                     <span class="text-<?php echo $row['id']; ?>">
-                    <?php $row['text'] = preg_replace('/(?<!\S)#([0-9a-zA-Z]+)/', '<a href="/hashtag/$1">#$1</a>', $row['text']); ?>
+                        <?php $row['text'] = preg_replace('/(?<!\S)#([0-9a-zA-Z]+)/', '<a href="/hashtag/$1">#$1</a>', $row['text']); ?>
 
                         <?php echo $row['text']; ?>
-                    </span> 
+                    </span>
                     <small class="date date-<?php echo $row['id']; ?> float-right" data-date="<?php echo date("Y-m-d", strtotime($row['date'])) ?>">
                         <?php echo date("D d M Y", strtotime($row['date'])); ?>
                     </small>
@@ -85,6 +85,7 @@
             <?php
             }
             ?>
+            </ul></div>
         </div>
     </div>
     <nav class="navbar fixed-bottom navbar-expand navbar-dark bg-dark">
@@ -96,7 +97,7 @@
         <div class="collapse navbar-collapse" id="navbarsExample02">
             <ul class="navbar-nav">
                 <li class="nav-item dropup">
-                    <a class="nav-link navbar-brand active dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Weekloggr</a>
+                    <a class="nav-link navbar-brand active dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Weekloggr </a>
                     <div class="dropdown-menu" aria-labelledby="dropdown10">
                         <a class="dropdown-item" href="#">Settings</a>
                     </div>
@@ -104,7 +105,7 @@
             </ul>
             <form method="POST" name="addweeklog" action="<?php echo $base_url; ?>/addlog">
                 <input class="form-control text" name="weeklog" type="text" placeholder="Log work done">
-                <input class="form-control d-none dateupdate" name="date" type="date"  value="">
+                <input class="form-control d-none dateupdate" name="date" type="date" value="">
                 <button type="submit" class="d-none">Skicka</button>
             </form>
         </div>
